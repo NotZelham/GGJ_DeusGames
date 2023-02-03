ac781cf8ba4d9c6cfe6a400b0a8e0f2e599e52cd
NotZelham <lochetdenis99@gmail.com>
2023-02-04 00:51:03 +0100

    plant Love qui dance

--- Assets/Scripts/Plant/LovePlant.cs
+++ Assets/Scripts/Plant/LovePlant.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class LovePlant : MonoBehaviour
 {
+    Animator anim;
+    [SerializeField] Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        anim.SetFloat("Dist", int.MaxValue);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetFloat("Dist", Vector3.Distance(transform.position, player.position));
     }
 }
