4ab6ff0a403562ae22b4e62d5907bd9f61590cfc
NotZelham <lochetdenis99@gmail.com>
2023-02-04 01:30:56 +0100

    ajout de l'interraction avec la Fleur Love

--- Assets/Scripts/Plant/LovePlant.cs
+++ Assets/Scripts/Plant/LovePlant.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
-public class LovePlant : MonoBehaviour
+public class LovePlant : MonoBehaviour, IInteractable
 {
     Animator anim;
+    bool isSauvage;
+    [SerializeField] Item item;
     [SerializeField] Transform player;
+
+    public void DoInteraction()
+    {
+       Item it = InventoryController.Instance.inventory.AddItem(item);
+       
+       if (it == null)
+           Destroy(gameObject);
+       else
+           item = it;
+    }
+
+    public void SetInteractionText()
+    {
+        ActionUI.Instance.SetVisible();
+
+        ActionUI.Instance.SetText($"Press E to pick {item.displayName}");
+    }
+    private void Awake()
+    {
+
+        item.currentStackAmount = 1;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        isSauvage = true;
         anim = GetComponent<Animator>();
         anim.SetFloat("Dist", int.MaxValue);
     }
